5c21db346d1b35841cb7b23f4c92930ffc5ff21d
pandi2019 <58973982+pandi2019@users.noreply.github.com>
2026-05-02 12:02:48 +0700

    Clean up Supabase configuration comments
    Removed commented-out Supabase configuration examples.

--- barbershop/config.js
+++ barbershop/config.js
@@ -1,11 +1,6 @@
 window.YMW_CONFIG = {
   appName: "YM-W HAIRCUT",
   outletName: "YM-W HAIRCUT Cabang Utama",
-
-  // Isi dua nilai ini kalau sudah punya project Supabase.
-  // Contoh:
-  // supabaseUrl: "https://xxxx.supabase.co",
-  // supabaseAnonKey: "eyJhbGciOi...",
  supabaseUrl: "https://grvpnksgjtmguautmcsu.supabase.co/rest/v1/",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdydnBua3NnanRtZ3VhdXRtY3N1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2OTcxNDUsImV4cCI6MjA5MzI3MzE0NX0.oN29Mo5rR9ISqWEQ1Iwx6MbpIcJ7qKziF83UqTX8VLs",
 };
