fdae4bf7dc9f9c4c6fc4069abed4e1e3fcb86d30
pandi2019 <58973982+pandi2019@users.noreply.github.com>
2026-05-02 11:52:34 +0700

    Set Supabase URL and anon key in config.js
    Updated Supabase configuration with actual URL and anon key.

--- barbershop/config.js
+++ barbershop/config.js
@@ -6,6 +6,6 @@ window.YMW_CONFIG = {
   // Contoh:
   // supabaseUrl: "https://xxxx.supabase.co",
   // supabaseAnonKey: "eyJhbGciOi...",
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+ supabaseUrl: "https://grvpnksgjtmguautmcsu.supabase.co/rest/v1/",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdydnBua3NnanRtZ3VhdXRtY3N1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2OTcxNDUsImV4cCI6MjA5MzI3MzE0NX0.oN29Mo5rR9ISqWEQ1Iwx6MbpIcJ7qKziF83UqTX8VLs",
 };
